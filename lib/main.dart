43ffd2ed1f8c8c88bb32a0ecddc5ce291679012e
Mohammed Azim Moula <azimmoula1@gmail.com>
2025-01-18 12:49:32 +0530

    Complete Portfolio

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:dnd/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Design And Dev',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        textTheme: GoogleFonts.albertSansTextTheme(),
+      ),
+      home: HomePage(),
+    );
+  }
+}
